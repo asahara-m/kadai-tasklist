b5e1da5b4f795e168582d921999ad87f4f36c94f
asaharamie <asaharamie@asaharamienoair.lan>
2024-04-29 01:48:30 +0900

    Index/NewServletの修正

--- src/controllers/tsk_IndexServlet.java
+++ src/controllers/tsk_IndexServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -35,9 +36,15 @@ public class tsk_IndexServlet extends HttpServlet {
         EntityManager em = DBUtil.createEntityManager();
 
         List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class).getResultList();
-        response.getWriter().append(Integer.valueOf(tasks.size()).toString());
 
         em.close();
+
+        request.setAttribute("tasks", tasks);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views2/tasks2/index2.jsp");
+        rd.forward(request, response);
+
+
     }
 
 }
