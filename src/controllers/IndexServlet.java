bef8d627ba0b61249a5a6b916857921c6a5ef63e
asaharamie <asaharamie@asaharamienoair.lan>
2024-04-29 15:42:54 +0900

    クラス名の修正

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -18,13 +18,13 @@ import utils.DBUtil;
  * Servlet implementation class tsk_IndexServlet
  */
 @WebServlet("/index")
-public class tsk_IndexServlet extends HttpServlet {
+public class IndexServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public tsk_IndexServlet() {
+    public IndexServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
